b188b3a91220be94068377752f09b3241b15adfc
ayu135 <ayushsharma06@gmail.com>
2016-03-07 11:18:54 -0600

    Zed demo added

---
+++ build/CMakeFiles/ZED_PROJECT.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/ZED_PROJECT.dir/src/depth_zed.o"
+  "ZED_PROJECT.pdb"
+  "ZED_PROJECT"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/ZED_PROJECT.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
